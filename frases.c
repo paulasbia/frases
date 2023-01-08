c6be3d14dd77f65749747a396557288aee82a372
Paula Biazotto <pds_paula@hotmail.com>
2023-01-08 10:57:56 +0000

    random numbers generation is working

--- frases.c
+++ frases.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
+	srand(time(0));
+    int frasealeatoria = rand() % 10;
+    printf("%d\n", frasealeatoria);
 	
 	return 0;
 }
